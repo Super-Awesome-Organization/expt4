082379b38103ea9898214dd6700cb8da3ae1370d
nismo00720 <60679106+nismo00720@users.noreply.github.com>
2021-09-21 08:53:07 -0400

    Update trojan_5.v

--- trojan_5.v
+++ trojan_5.v
@@ -2,7 +2,7 @@
 //Experiement 4 Part 2
 //9/17/21
 
-//The trojan.v file to determine if the trigger condition is met
+//The trojan_5.v file to determine if the trigger condition is met
 // it will return the pay load (modified key) or not.
 // The trigger condition can be change by changing the value of the condition register value
 // The tigger is decmial 5
@@ -12,8 +12,8 @@ module  trojan_5(payload, key, trigger);
 input	[55:0]	    key;
   input	[1:32]	    trigger;
 output	[55:0]	    payload;
-  reg     [3:0]     condition = 4'b1010; // lab doc the trigger is b01010 but due to big endiness of the code it has to be flipped
+  reg     [3:0]     condition = 4'b0101; 
 
-  assign payload = (trigger[1:4] == condition[3:0]) ? {key[55:1], ~key[0]} : key[55:0];
+  assign payload = (trigger[29:32] == condition[3:0]) ? {key[55:1], ~key[0]} : key[55:0]; // LSB is [32]. When the trigger condition is met key[0] is inverted
 
 endmodule
